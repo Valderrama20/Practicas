8c9163ac4fc4e1d396152a985b848b1b141a03d9
Valderrama20 <jose23122009@gmai.com>
2023-04-04 17:18:53 -0300

    practicas

--- dia 2/script.js
+++ dia 2/script.js
@@ -1,17 +1,31 @@
 const next = document.getElementById("next")
 const prev = document.getElementById("prev")
+const circle = document.querySelectorAll(".number")
+const progress = document.getElementById("progress")
+let actives = document.querySelectorAll('.active')
 
 let num = 1
 
-num === 1 ? prev.disabled = true: prev.disabled = false
+
+const onclick = (value) => {
+    value ? num++ : num--
     
-prev.onclick = () => {
-    num--
-    console.log(num)
+    progress.value =  actives.length * 100 /circle.length
+
     num === 1 ? prev.disabled = true: prev.disabled = false
+    num === 4 ? next.disabled = true: next.disabled = false
+    
+
+    circle.forEach((element, index) => {
+        num > index
+        ?element.classList.add("active")
+        :element.classList.remove("active")
+
+}) 
+
 }
-next.onclick = () => {
-    num++
-    console.log(num)
-    num === 1 ? prev.disabled = true: prev.disabled = false
-}
+
+next.onclick =() => onclick(true)
+prev.onclick = () => onclick(false)
+
+
