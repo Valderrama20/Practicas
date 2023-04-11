52f92e10d097ddc98fe80ca28db7054b72f30229
Valderrama20 <jose23122009@gmai.com>
2023-04-11 20:57:29 -0300

    dia 4 listo

--- dia 4/script.js
+++ dia 4/script.js
@@ -1,7 +1,26 @@
 var btn = document.querySelector(".btn");
 var input = document.querySelector(".input");
 var div = document.querySelector(".div");
+
+let view = true
+
 btn.onclick = function () {
+    if(view){
+    
     div.classList.add("active");
     input.classList.add("none");
-};
+    view = !view
+    
+}
+    else if(!view) {
+        
+        div.classList.remove("active");
+    input.classList.remove("none");
+    view = !view
+   
+
+}
+
+    
+    }
+
