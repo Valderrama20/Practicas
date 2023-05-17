21de2d389f0851582b2d36785f209aedcc409504
Valderrama20 <jose23122009@gmai.com>
2023-05-16 21:54:08 -0300

    dia 11 listo

---
+++ dia 11/script.js
@@ -0,0 +1,28 @@
+
+const insert = document.getElementById('insert')
+
+
+window.addEventListener('keydown' , (event) => {
+
+  insert.innerHTML = `
+   
+  <div class="key">
+   ${event.code == "Space" ? "Space": event.key}
+   <small>event.Key</small>
+   
+  </div>
+
+  <div class="key">
+   ${event.keyCode}
+   <small>event.KeyCode</small>
+   
+  </div>
+  <div class="key">
+   ${event.code}
+   <small>event.code</small>
+   
+  </div>
+  
+  `
+
+})
--- dia 3 completar/script.js
+++
@@ -1,5 +0,0 @@
-var btn = document.getElementById("btn");
-btn.onclick = function () {
-    console.log("jose");
-};
-var jose = "jose";
